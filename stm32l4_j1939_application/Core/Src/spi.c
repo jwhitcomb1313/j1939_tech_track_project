4d9295e940e4e8ad35366754c22106d86f55e370
Justin Whitcomb <justin.whitcomb@distek.com>
2024-06-21 09:05:03 -0500

    added basic project created from cubemx and datasheet folder

---
+++ stm32l4_j1939_application/Core/Src/spi.c
@@ -0,0 +1,14 @@
+/*
+ * spi.c
+ *
+ *  Created on: 6/21/2024
+ *      Author: Justin Whitcomb
+ */
+
+#include "spi.h"
+
+
+char spi_write_buffer[20]; 
+char spi_read_buffer[20]; 
+
+
---
+++ stm32l4_j1939_application/Core/Src/spi.h
@@ -0,0 +1,14 @@
+/*
+ * spi.h
+ *
+ *  Created on: 6/21/2024
+ *      Author: Justin Whitcomb
+ */
+
+#include "main.h"
+
+typedef enum 
+{
+    SPI_OK,
+    SPI_ERROR
+}spi_error_t; 
