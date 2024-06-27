9139ffdfb0bc9aa9011770995c349c100275131e
Justin Whitcomb <justin.whitcomb@distek.com>
2024-06-27 09:37:01 -0500

    added documentation and altered some functions

--- stm32l4_j1939_application/Core/Src/mcp2515.c
+++ stm32l4_j1939_application/Core/Src/mcp2515.c
@@ -19,11 +19,32 @@ static void SPI_RxBuffer(uint8_t *buffer, uint8_t length);
 /******************** ******************** ***********************/
 /******************** Function Definitions ***********************/
 /******************** ******************** ***********************/
+
+/******************************************************************************/
+/*!
+   \fn      void MCP2515_Init(void)
+   \brief   This function inits settings for the mcp2515 
+   \param   None
+   \return  None
+
+    @{
+*/
+/******************************************************************************/
 void MCP2515_Init(void)
 {
   MCP2515_SetConfigurationMode(); 
 }
 
+/******************************************************************************/
+/*!
+   \fn      bool MCP2515_SetConfigurationMode(void)
+   \brief   This function sets the mcp2515 into configuration mode 
+   \param   None
+   \return  bool: determines if the mode was set or not
+
+    @{
+*/
+/******************************************************************************/
 bool MCP2515_SetConfigurationMode(void)
 {
   MCP2515_WriteByte(MCP2515_CANCTRL, MODE_CONFIGURATION); 
@@ -38,6 +59,16 @@ bool MCP2515_SetConfigurationMode(void)
   return false;
 }
 
+/******************************************************************************/
+/*!
+   \fn      bool MCP2515_SetNormalMode(void)
+   \brief   This function sets the mcp2515 into normal mode 
+   \param   None
+   \return  bool: determines if the mode was set or not
+
+    @{
+*/
+/******************************************************************************/
 bool MCP2515_SetNormalMode(void)
 {
   MCP2515_WriteByte(MCP2515_CANCTRL, MODE_NORMAL); 
@@ -51,6 +82,17 @@ bool MCP2515_SetNormalMode(void)
   }
   return false;
 }
+
+/******************************************************************************/
+/*!
+   \fn      bool MCP2515_SetLoopbackMode(void)
+   \brief   This function sets the mcp2515 into loopback mode 
+   \param   None
+   \return  bool: determines if the mode was set or not
+
+    @{
+*/
+/******************************************************************************/
 
 bool MCP2515_SetLoopbackMode(void)
 {
@@ -66,37 +108,69 @@ bool MCP2515_SetLoopbackMode(void)
   return false; 
 }
 
-// Write a single byte to the MCP2515
+/******************************************************************************/
+/*!
+   \fn      void MCP2515_WriteByte(uint8_t address, uint8_t data)
+   \brief   This function writes a single byte to a register at the selected adddress 
+   \param   uint8_t address: address to write the byte 
+   \param   uint8_t data: byte that is to be written
+   \return   None
+
+    @{
+*/
+/******************************************************************************/
 void MCP2515_WriteByte(uint8_t address, uint8_t data)
 {
   MCP2515_CS_LOW();  
 
-  SPI_Tx(INSTRUCTION_WRITE);
+  SPI_Tx((uint8_t)INSTRUCTION_WRITE);
   SPI_Tx(address);
   SPI_Tx(data);  
   
   MCP2515_CS_HIGH();
 }
 
-// Write a series of bytes to the MCP2515
-void MCP2515_WriteByteSequence(uint8_t address, uint8_t* data, uint8_t length)
+/******************************************************************************/
+/*!
+   \fn      void MCP2515_WriteTxBuffer(tx_buffer_instruct_t instruction, uint8_t* data, uint8_t length)
+   \brief   This function loads data into one of the tx buffers, overhead is reduced by includeing the 
+            instruction into the address. 
+   \param   tx_buffer_instruct_t instruction: This type is both the address and write buffer instruction with 
+            6 possible instructions. This points to the ID or data adress of any of the 3 tx buffers
+   \param   uint8_t* data: Pointer to the data that is to be written to the buffer  
+   \param   uint8_t length: Length of the data  
+   \return   None
+
+    @{
+*/
+/******************************************************************************/
+void MCP2515_WriteTxBuffer(tx_buffer_instruct_t instruction, uint8_t* data, uint8_t length)
 {
   MCP2515_CS_LOW();
 
-  SPI_Tx(MCP2515_WRITE);
-  SPI_Tx(address);
+  SPI_Tx((uint8_t)instruction);
   SPI_TxBuffer(data, length);
 
   MCP2515_CS_HIGH();
 }
 
+/******************************************************************************/
+/*!
+   \fn      uint8_t MCP2515_ReadByte(uint8_t address)
+   \brief   This function reads a single byte from a register at the selected adddress 
+   \param   uint8_t address: address to read the byte 
+   \return  uint8_t retVal: byte at the given address
+
+    @{
+*/
+/******************************************************************************/
 uint8_t MCP2515_ReadByte(uint8_t address)
 {
     uint8_t retVal = 0; 
 
     MCP2515_CS_LOW();
   
-    SPI_Tx(MCP2515_READ);
+    SPI_Tx((uint8_t)INSTRUCTION_READ);
     SPI_Tx(address);
     retVal = SPI_Rx();
   
@@ -105,13 +179,37 @@ uint8_t MCP2515_ReadByte(uint8_t address)
     return retVal;
 }
 
-void MCP2515_ReadByteSequence(uint8_t instruction, uint8_t address, uint8_t* data, uint8_t length)
+/******************************************************************************/
+/*!
+   \fn      void MCP2515_ReadRxBuffer(rx_buffer_instruct_t instruction, uint8_t* data, uint8_t length)
+   \brief   This function reads data from one of the rx buffers, overhead is reduced by includeing the 
+            instruction into the address. 
+   \param   rx_buffer_instruct_t instruction: This type is both the address and read buffer instruction with 
+            4 possible instructions. 
+   \param   uint8_t* data: Pointer that the data will be written to  
+   \param   uint8_t length: Length of the data  
+   \return   None
+
+    @{
+*/
+/******************************************************************************/
+void MCP2515_ReadRxBuffer(rx_buffer_instruct_t instruction, uint8_t* data, uint8_t length)
 {
     SPI_Tx(instruction); 
     SPI_RxBuffer(data, length); 
 }
 
-/* read RX STATUS register */
+/******************************************************************************/
+/*!
+   \fn      uint8_t MCP2515_GetRxStatus(void)
+   \brief   This function is a quick polling command that indicates filter match 
+            and message type (either standard, extended, and/or remote) of received message  
+   \param   None. 
+   \return  uint8_t retVal: Rx status byte
+
+    @{
+*/
+/******************************************************************************/
 uint8_t MCP2515_GetRxStatus(void)
 {
   uint8_t retVal;
@@ -119,6 +217,39 @@ uint8_t MCP2515_GetRxStatus(void)
   MCP2515_CS_LOW();
   
   SPI_Tx(MCP2515_RX_STATUS);
+  retVal = SPI_Rx();
+        
+  MCP2515_CS_HIGH();
+  
+  return retVal;
+}
+
+/******************************************************************************/
+/*!
+   \fn      uint8_t MCP2515_GetReadStatus(void)
+   \brief   This function is a quick polling command that reads several status bits 
+            for transmit and receive functions 
+            7: TX2IF (CANINTF[4])
+            6: TXREQ (TXB2CNTRL[3])
+            5: TX1IF (CANINTF[3])
+            4: TXREQ (TXB1CNTRL[3])
+            3: TX0IF (CANINTF[2])
+            2: TXREQ (TXB0CNTRL[3])
+            1: RX1IF (CANINTF[1])
+            0: RX0IF (CANINTF[0]) 
+   \param   None. 
+   \return  uint8_t retVal: Read status byte
+
+    @{
+*/
+/******************************************************************************/
+uint8_t MCP2515_GetReadStatus(void)
+{
+  uint8_t retVal;
+  
+  MCP2515_CS_LOW();
+  
+  SPI_Tx(MCP2515_READ_STATUS);
   retVal = SPI_Rx();
         
   MCP2515_CS_HIGH();
--- stm32l4_j1939_application/Core/Src/mcp2515.h
+++ stm32l4_j1939_application/Core/Src/mcp2515.h
@@ -134,14 +134,112 @@ typedef enum
     INSTRUCTION_RESET       = 0xC0
 }mcp2515_instruction_t;
 
+typedef enum 
+{
+    MCP_RXF0SIDH = 0x00,
+    MCP_RXF0SIDL = 0x01,
+    MCP_RXF0EID8 = 0x02,
+    MCP_RXF0EID0 = 0x03,
+    MCP_RXF1SIDH = 0x04,
+    MCP_RXF1SIDL = 0x05,
+    MCP_RXF1EID8 = 0x06,
+    MCP_RXF1EID0 = 0x07,
+    MCP_RXF2SIDH = 0x08,
+    MCP_RXF2SIDL = 0x09,
+    MCP_RXF2EID8 = 0x0A,
+    MCP_RXF2EID0 = 0x0B,
+    MCP_CANSTAT  = 0x0E,
+    MCP_CANCTRL  = 0x0F,
+    MCP_RXF3SIDH = 0x10,
+    MCP_RXF3SIDL = 0x11,
+    MCP_RXF3EID8 = 0x12,
+    MCP_RXF3EID0 = 0x13,
+    MCP_RXF4SIDH = 0x14,
+    MCP_RXF4SIDL = 0x15,
+    MCP_RXF4EID8 = 0x16,
+    MCP_RXF4EID0 = 0x17,
+    MCP_RXF5SIDH = 0x18,
+    MCP_RXF5SIDL = 0x19,
+    MCP_RXF5EID8 = 0x1A,
+    MCP_RXF5EID0 = 0x1B,
+    MCP_TEC      = 0x1C,
+    MCP_REC      = 0x1D,
+    MCP_RXM0SIDH = 0x20,
+    MCP_RXM0SIDL = 0x21,
+    MCP_RXM0EID8 = 0x22,
+    MCP_RXM0EID0 = 0x23,
+    MCP_RXM1SIDH = 0x24,
+    MCP_RXM1SIDL = 0x25,
+    MCP_RXM1EID8 = 0x26,
+    MCP_RXM1EID0 = 0x27,
+    MCP_CNF3     = 0x28,
+    MCP_CNF2     = 0x29,
+    MCP_CNF1     = 0x2A,
+    MCP_CANINTE  = 0x2B,
+    MCP_CANINTF  = 0x2C,
+    MCP_EFLG     = 0x2D,
+    MCP_TXB0CTRL = 0x30,
+    MCP_TXB0SIDH = 0x31,
+    MCP_TXB0SIDL = 0x32,
+    MCP_TXB0EID8 = 0x33,
+    MCP_TXB0EID0 = 0x34,
+    MCP_TXB0DLC  = 0x35,
+    MCP_TXB0DATA = 0x36,
+    MCP_TXB1CTRL = 0x40,
+    MCP_TXB1SIDH = 0x41,
+    MCP_TXB1SIDL = 0x42,
+    MCP_TXB1EID8 = 0x43,
+    MCP_TXB1EID0 = 0x44,
+    MCP_TXB1DLC  = 0x45,
+    MCP_TXB1DATA = 0x46,
+    MCP_TXB2CTRL = 0x50,
+    MCP_TXB2SIDH = 0x51,
+    MCP_TXB2SIDL = 0x52,
+    MCP_TXB2EID8 = 0x53,
+    MCP_TXB2EID0 = 0x54,
+    MCP_TXB2DLC  = 0x55,
+    MCP_TXB2DATA = 0x56,
+    MCP_RXB0CTRL = 0x60,
+    MCP_RXB0SIDH = 0x61,
+    MCP_RXB0SIDL = 0x62,
+    MCP_RXB0EID8 = 0x63,
+    MCP_RXB0EID0 = 0x64,
+    MCP_RXB0DLC  = 0x65,
+    MCP_RXB0DATA = 0x66,
+    MCP_RXB1CTRL = 0x70,
+    MCP_RXB1SIDH = 0x71,
+    MCP_RXB1SIDL = 0x72,
+    MCP_RXB1EID8 = 0x73,
+    MCP_RXB1EID0 = 0x74,
+    MCP_RXB1DLC  = 0x75,
+    MCP_RXB1DATA = 0x76
+}mcp2515_register_t;
+
+typedef enum 
+{
+    TX_BUF_TXB0SIDH = 0x31,
+    TX_BUF_TXB0DATA = 0x36,
+    TX_BUF_TXB1SIDH = 0x41,
+    TX_BUF_TXB1DATA = 0x46,
+    TX_BUF_TXB2SIDH = 0x51,
+    TX_BUF_TXB2DATA = 0x56,
+}tx_buffer_instruct_t; 
+
+typedef enum
+{
+    RX_BUF_RXB0SIDH = 0x61,
+    RX_BUF_RXB0DATA = 0x66,
+    RX_BUF_RXB1SIDH = 0x71,
+    RX_BUF_MCP_RXB1DATA = 0x76
+}rx_buffer_instruct_t; 
 /******************* ********** ***********************/
 /******************* Prototypes ***********************/
 /******************* ********** ***********************/
 void MCP2515_Init(void); 
 void MCP2515_WriteByte(uint8_t address, uint8_t data);
-void MCP2515_WriteByteSequence(uint8_t address, uint8_t* data, uint8_t length);
+void MCP2515_WriteTxBuffer(tx_buffer_instruct_t instruction, uint8_t* data, uint8_t length);
 uint8_t MCP2515_ReadByte(uint8_t address); 
-void MCP2515_ReadByteSequence(uint8_t instruction, uint8_t address, uint8_t* data, uint8_t length); 
+void MCP2515_ReadRxBuffer(rx_buffer_instruct_t instruction, uint8_t address, uint8_t* data, uint8_t length); 
 uint8_t MCP2515_GetRxStatus(void); 
 bool MCP2515_SetConfigurationMode(void);
 bool MCP2515_SetNormalMode(void); 
